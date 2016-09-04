cb9f06825d2f781a1ae5325bfa04d094eb4758c4
kodakodu <kodakodu@gmail.com>
2016-09-04 22:16:58 +0200

    Ajout de Bootstrap

---
+++ application/views/accueil.php
@@ -0,0 +1,5 @@
+<div style=" margin-bottom: 50px; margin-top: 50px; text-align: center; margin-left: 100px;background-color: #F5F6CE ; width: 800px;">
+
+	Novitates autem si spem adferunt, ut tamquam in herbis non fallacibus fructus appareat, non sunt illae quidem repudiandae, vetustas tamen suo loco conservanda; maxima est enim vis vetustatis et consuetudinis. Quin in ipso equo, cuius modo feci mentionem, si nulla res impediat, nemo est, quin eo, quo consuevit, libentius utatur quam intractato et novo. Nec vero in hoc quod est animal, sed in iis etiam quae sunt inanima, consuetudo valet, cum locis ipsis delectemur, montuosis etiam et silvestribus, in quibus diutius commorati sumus.
+
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,66 @@
+<html>
+	<head>
+		<title>Budo91</title>
+		<link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php echo base_url();?>assets/css/styles.css" rel="stylesheet">
+		<link rel="icon" href="<?php echo base_url();?>assets/images/favicon.ico" type="image/x-icon"/>
+	</head>
+	<body>
+		<div class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="<?php echo base_url();?>index.php">Accueil</a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<!--<li class="active"><a href="http://localhost:8095/Budo91/index.php">Accueil</a></li>-->
+						<li>
+							<a href="<?php echo base_url();?>index.php/disciplines">Disciplines</a>
+						</li>
+						<li>
+							<a href="<?php echo base_url();?>index.php/clubs">Clubs</a>
+						</li>
+						<li>
+							<a href="<?php echo base_url();?>index.php/contact">Contact</a>
+						</li>
+						<li>
+							<a href="<?php echo base_url();?>index.php/admin">Admin</a>
+
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="<?php echo base_url();?>index.php/about">A propos</a>
+						</li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+<!--<div class="btn-group">
+								<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Disciplines <span class="caret"></span>
+								</button>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="#">Action</a>
+									</li>
+									<li>
+										<a href="#">Another action</a>
+									</li>
+									<li>
+										<a href="#">Something else here</a>
+									</li>
+									<li role="separator" class="divider"></li>
+									<li>
+										<a href="#">Separated link</a>
+									</li>
+								</ul>
+				</div>-->
+		<!--<h1><?php echo $title; ?></h1>
+		<a href="http://localhost:8095/Budo91/index.php/pages/accueil">Home</a>
+		<a href="http://localhost:8095/Budo91/index.php/pages/about">About</a>-->
